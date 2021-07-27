9c15c2c7bcf4f2a7ca78e0df178a126b7b97324a
xyzxyz1337 <44783727+xyzxyz1337@users.noreply.github.com>
2021-07-27 13:33:23 +0300

    Fixed missprint in project name

--- build-exe.bat
+++ build-exe.bat
@@ -4,14 +4,10 @@ rem for build use pyinstaller version >= 4.3
 pip install -r .\requirements.txt --cache-dir .\pypackages
 pyinstaller.exe --noconfirm --log-level ERROR ^
                 --onefile --uac-admin ^
-                --clean --name Installer-X-CON ^
+                --clean --name Installer-CON-X ^
                 --paths .\pypackages .\setup.py
 
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" setup.iss
 
-echo
-echo
-echo
-echo "Installer store in setup directory"
-
+echo "Installer store in `setup` directory"
 timeout 30
